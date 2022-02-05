9081e3566fe48b4fc3b69a2bf2ca8c1260833db9
Prithviraj-cyber <anandsinghchauhan03646@gmail.com>
2022-02-05 10:18:30 -0500

    first commit

---
+++ Customer/footer.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+</head>
+<body>
+
+	<!-- ======= Footer ======= -->
+    <footer id="footer">
+        <div class="footer-top">
+            <div class="container">
+                <div class="row">
+
+                <div class="col-lg-3 col-md-6 footer-links">
+                    <h4>Company</h4>
+                    <ul>
+                        <li><i class="ion-ios-arrow-right"></i> <a href="#">Portfolio</a></li>
+                        <li><i class="ion-ios-arrow-right"></i> <a href="#">Reward Store</a></li>
+                        <li><i class="ion-ios-arrow-right"></i> <a href="#">Lucky Winner</a></li>
+                        <li><i class="ion-ios-arrow-right"></i> <a href="#">IWC (Agent)</a></li>
+                        <li><i class="ion-ios-arrow-right"></i> <a href="#">Franchise Store</a></li>
+                        <li><i class="ion-ios-arrow-right"></i> <a href="#">Brand Ambassador</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-3 col-md-6 footer-links">
+                    <h4>Request</h4>
+                    <ul>
+                        <li><i class="ion-ios-arrow-right"></i> <a href="#">Freelance</a></li>
+                        <li><i class="ion-ios-arrow-right"></i> <a href="#">IWC (Agent)</a></li>
+                        <li><i class="ion-ios-arrow-right"></i> <a href="#">Franchise Store</a></li>
+                        <li><i class="ion-ios-arrow-right"></i> <a href="#">Service Partner</a></li>
+                        <li><i class="ion-ios-arrow-right"></i> <a href="#">Influencer Partner</a></li>
+                        <li><i class="ion-ios-arrow-right"></i> <a href="#">Join Startup Team</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-3 col-md-6 footer-links">
+                    <h4>Booking</h4>
+                    <ul>
+                        <li><i class="ion-ios-arrow-right"></i> <a href="#">Vloog Shoot</a></li>
+                        <li><i class="ion-ios-arrow-right"></i> <a href="#">Photo Shoot</a></li>
+                        <li><i class="ion-ios-arrow-right"></i> <a href="#">Video Shoot</a></li>
+                        <li><i class="ion-ios-arrow-right"></i> <a href="#">Create Catalog</a></li>
+                        <li><i class="ion-ios-arrow-right"></i> <a href="#">Create Template</a></li>
+                        <li><i class="ion-ios-arrow-right"></i> <a href="#">Create Advertisement</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-3 col-md-6 footer-links">
+                    <h4>Policy</h4>
+                    <ul>
+                        <li><i class="ion-ios-arrow-right"></i> <a href="#">Legal</a></li>
+                        <li><i class="ion-ios-arrow-right"></i> <a href="#">Privacy</a></li>
+                        <li><i class="ion-ios-arrow-right"></i> <a href="#">Cookies</a></li>
+                        <li><i class="ion-ios-arrow-right"></i> <a href="#">Disclaimer</a></li>
+                        <li><i class="ion-ios-arrow-right"></i> <a href="#">Terms Of Use</a></li>
+                        <li><i class="ion-ios-arrow-right"></i> <a href="#">Advertisement</a></li>
+                    </ul>
+                </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+        <div class="copyright">
+            &copy; Copyright <strong>IndianWala</strong>. All Rights Reserved
+        </div>
+        <div class="credits">
+            Designed by <a href="https://www.aventainfotech.com/">Aventa Infotech</a>
+        </div>
+        </div>
+    </footer><!-- End Footer -->
+
+    <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+
+
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+</body>
+</html>
